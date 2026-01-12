9781489f7a79a547c57d040461abdbdaf520b8d0
modreness <zan.andjic@gmail.com>
2026-01-12 10:39:21 +0100

    Update help-box.php

--- app/views/partials/help-box.php
+++ app/views/partials/help-box.php
@@ -5,8 +5,8 @@
     <div class="info-text">
         <h3>Imate poteškoća sa korištenjem aplikacije?</h3>
         <p>Kontaktirajte nas putem email adrese: 
-            <a href="mailto:support@spes.ba">support@spes.ba</a>
+            <a href="mailto:info@galopdigital.com">info@galopdigital.com</a>
         </p>
-        <p>ili na broj <a href="tel:123456789">123 456 789</a></p>
+        <p>ili na broj <a href="tel:062905364">062 905 364</a></p>
     </div>
 </div>
